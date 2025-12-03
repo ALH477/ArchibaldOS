53935850465b8786d81157a1dc16c3ed400dd1ac
Asher <146795551+ALH477@users.noreply.github.com>
2025-12-03 07:20:20 -0800

    Update base.nix

--- modules/base.nix
+++ modules/base.nix
@@ -1,9 +1,7 @@
+# modules/base.nix
 { config, pkgs, lib, ... }:
 
 {
-  # Base system configuration for all variants
-  
-  # Locale & Timezone
   time.timeZone = lib.mkDefault "UTC";
   i18n.defaultLocale = "en_US.UTF-8";
   
@@ -12,10 +10,8 @@
     keyMap = lib.mkDefault "us";
   };
 
-  # Power management
   powerManagement.enable = true;
 
-  # SSH for remote management
   services.openssh = {
     enable = lib.mkDefault true;
     settings = {
@@ -24,19 +20,16 @@
     };
   };
 
-  # Basic system packages
   environment.systemPackages = with pkgs; [
     vim wget curl git htop
     usbutils pciutils
   ];
 
-  # Nix settings
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
     auto-optimise-store = true;
   };
 
-  # Automatic garbage collection
   nix.gc = {
     automatic = lib.mkDefault true;
     dates = "weekly";
