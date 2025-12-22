ec3fc96c085ae801ef458e4b7425408f7b34608f
Asher <146795551+ALH477@users.noreply.github.com>
2025-12-22 10:06:53 -0800

    Update hydramesh-plugins.nix

--- modules/hydramesh-plugins.nix
+++ modules/hydramesh-plugins.nix
@@ -1,11 +1,12 @@
 # modules/hydramesh-plugins.nix
 { pkgs, lib, ... }: {
   environment.etc = {
-    "hydramesh/plugins/lora.lisp".source = ./../HydraMesh/plugins/lora.lisp;
-    "hydramesh/plugins/zigbee.lisp".source = ./../HydraMesh/plugins/zigbee.lisp;
-    "hydramesh/plugins/can-bus.lisp".source = ./../HydraMesh/plugins/can-bus.lisp;
-    "hydramesh/plugins/serial.lisp".source = ./../HydraMesh/plugins/serial.lisp;
+    "hydramesh/plugins/lora.lisp".source = ./../HydraMesh/plugins/lorawan-transport.lisp;
+    "hydramesh/plugins/zigbee.lisp".source = ./../HydraMesh/plugins/zigbee-transport.lisp;
+    "hydramesh/plugins/can-bus.lisp".source = ./../HydraMesh/plugins/can-transport.lisp;
+    "hydramesh/plugins/serial.lisp".source = ./../HydraMesh/plugins/serial-transport.lisp;
     "hydramesh/plugins/thunderbolt-transport.lisp".source = ./../HydraMesh/plugins/thunderbolt-transport.lisp;
+    "hydramesh/plugins/thunderbolt-transport-tests.lisp".source = ./../HydraMesh/plugins/thunderbolt-transport-tests.lisp;
     # Add more as needed
   };
 
